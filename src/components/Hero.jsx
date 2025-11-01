7e081bcaff65db5e37d127fc1d9d0bea553a1cc6
David Hundley <david.k.hundley@gmail.com>
2025-10-31 20:20:13 -0500

    Fixing the padding of the hero section for mobile displays

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
       <div className="absolute inset-0 -z-10 bg-white/60" />
       
   { /* Setting the main text content of the hero section */ }
-  <div className="max-w-5xl py-32 sm:py-48 lg:py-56 pl-8 sm:pl-40 font-montserrat">
+  <div className="max-w-5xl py-32 sm:py-48 lg:py-56 px-4 sm:px-8 md:px-16 lg:pl-40 font-montserrat">
         <div className="text-left">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl font-lulo-bold">
             FREEING PEOPLE TO DO THE WORK THEY LOVE THROUGH REGULATION + CONNECTION
