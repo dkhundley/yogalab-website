469acc2330d23fb3ccc2c22b3c59ec1788fe3d0c
David Hundley <david.k.hundley@gmail.com>
2025-10-31 22:49:05 -0500

    feat: Refactor App component to use AppContent for better structure and add favicon support based on active brand

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BrandProvider } from './context/BrandContext';
+import { BrandProvider, useBrand, BRANDS } from './context/BrandContext';
 import BrandSwitcher from './components/BrandSwitcher';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,26 +12,45 @@ import Contact from './pages/Contact';
 import FAQ from './pages/FAQ';
 import Book from './pages/Book';
 
+function AppContent() {
+  const { activeBrand } = useBrand();
+
+  useEffect(() => {
+    document.title = activeBrand === BRANDS.LAB_COFFEE ? 'Lab Coffee' : 'Yoga Lab';
+    
+    const favicon = document.querySelector("link[rel='icon']");
+    if (favicon) {
+      favicon.href = activeBrand === BRANDS.LAB_COFFEE 
+        ? '/icons/lab-coffee-favicon.svg' 
+        : '/icons/yoga-lab-favicon.svg';
+    }
+  }, [activeBrand]);
+
+  return (
+    <div className="min-h-screen bg-white">
+      <BrandSwitcher />
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/yoga" element={<Yoga />} />
+          <Route path="/coffee" element={<Coffee />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/book" element={<Book />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <BrandProvider>
-        <div className="min-h-screen bg-white">
-          <BrandSwitcher />
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/yoga" element={<Yoga />} />
-              <Route path="/coffee" element={<Coffee />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/book" element={<Book />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <AppContent />
       </BrandProvider>
     </Router>
   );
